c8b8f22d5d3288620a48546df260bb8f1f4cdc41
Eugene Rabenko <qwer09846@gmail.com>
2024-01-20 13:00:32 +0200

    fix naming

--- utils/credentials_operations.py
+++ utils/credentials_operations.py
@@ -3,5 +3,5 @@ from framework.page import Page
 
 class CredentialsOperation(Page):
     def enter_credential(self, credential, credential_field_xpath):
-        email_field = self.find_element(credential_field_xpath).clear()
-        email_field.send_keys(credential)
+        credential_field = self.find_element(credential_field_xpath).clear()
+        credential_field.send_keys(credential)
